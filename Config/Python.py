bcc3b09b1aaa959a65194974a6c8c1a4eaa65df5
Marcelo Vieira <39255607+SistemasVox@users.noreply.github.com>
2023-04-15 21:33:58 -0300

    Update Python.py

--- Config/Python.py
+++ Config/Python.py
@@ -10,6 +10,10 @@ pip cache purge
 #######################################################
 pip freeze > requirements.txt
 pip freeze --no-version > requirements.txt
+# Linux
+pip freeze | awk -F "==" '{print $1}' > requirements.txt
+# Windows
+pip freeze | ForEach-Object { $_.split('==')[0] } > requirements.txt
 
 pip install -r requirements.txt
 Exemplo de arquivo "requirements.txt":
